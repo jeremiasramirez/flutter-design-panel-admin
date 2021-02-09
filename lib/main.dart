5ca619871a506e255b6fdf30293e570c6b25f8bc
Jeremias ramirez <jereramirez499@gmail.com>
2021-02-09 16:51:56 -0400

    creando carpetas para pages y widgets

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:panel_flutter/pages/home.page.dart';
+
+void main() {
+  runApp(PanelApp());
+}
+
+class PanelApp extends StatelessWidget {
+ 
+ 
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      initialRoute: "/",
+      routes: {
+        "/" : (context) =>  new HomePage()
+      }
+
+    );
+
+
+  }
+}
+ 
---
+++ lib/pages/home.page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatelessWidget{
+
+
+  
+  Widget build(context){
+
+    return MaterialApp(
+
+      home:Scaffold(
+        appBar:AppBar(
+          title: Text("Panel")
+        )
+      )
+
+    );
+
+
+  }
+
+
+}
