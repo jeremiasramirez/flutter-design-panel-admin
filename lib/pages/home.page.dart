29dea0a341eeb5ba2af158cbb0ec4c404095a405
Jeremias ramirez <jereramirez499@gmail.com>
2021-03-07 01:48:26 -0400

    refactoring

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'package:panel_flutter/widgets/option.widget.dart';
+import 'package:panel_flutter/widgets/panel-user.widget.dart';
 
 
 class HomePage extends StatelessWidget{
@@ -11,13 +13,18 @@ class HomePage extends StatelessWidget{
       debugShowCheckedModeBanner: false,
       home:Scaffold(
         backgroundColor: Color.fromRGBO(10, 0, 50, 1),
+
         body:ListView(
           physics:BouncingScrollPhysics(),
+          
           children: [
-            
-            Table(
-              children: [
 
+            separated(50),
+            PanelUser(),
+            separated(50),
+
+            Table(
+              children: [ 
 
                 TableRow(
                   children: [
@@ -29,22 +36,18 @@ class HomePage extends StatelessWidget{
 
                 TableRow(
                   children: [
-                    Option(Icon(Icons.person_pin,  color:Colors.white, size:60.0), "Perfil"),
-                    Option(Icon(Icons.settings, color:Colors.white, size:60.0), "Settings")
-                  ]
-                ),
-                
-
-                TableRow(
-                  children: [
                     Option(Icon(Icons.person_search_sharp, color:Colors.white, size:60.0), "Search"),
                     Option(Icon(Icons.perm_contact_calendar_rounded, color:Colors.white, size:60.0), "Events")
                   ]
-                ) 
+                ),
 
       
-
-
+                TableRow(
+                  children: [
+                    Option(Icon(Icons.person_pin,  color:Colors.white, size:60.0), "Perfil"),
+                    Option(Icon(Icons.settings, color:Colors.white, size:60.0), "Settings")
+                  ]
+                ) 
               ],
             )
 
@@ -57,10 +60,14 @@ class HomePage extends StatelessWidget{
 
   }
 
+ 
 
+ SizedBox separated(double separate){
+   return SizedBox(height: separate);
+ }
 
 }
 
- 
+
 
 
